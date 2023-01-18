6996a426e5b0e8ade5f59fc801375fc906df749d
Сергей <s.v.yakovlev@yandex.ru>
2023-01-18 21:01:23 +0400

    Создал таблицы: probation, probationJournal, probationDate

--- src/main/resources/liquibase/scripts/user.sql
+++ src/main/resources/liquibase/scripts/user.sql
@@ -4,6 +4,32 @@
 
 
 --changeset yakovlev:1
+CREATE TABLE probation
+(
+    id         SERIAL,
+    userId     INTEGER,
+    petId      INTEGER,
+    dateBegin  TIMESTAMPTZ,
+    dateFinish TIMESTAMPTZ,
+    success    BOOLEAN,
+    result     TEXT
+);
 
+CREATE TABLE probationJournal
+(
+    id             SERIAL,
+    probationId    INTEGER,
+    date           TIMESTAMPTZ,
+    photoReceived  BOOLEAN,
+    reportReceived BOOLEAN
+);
+
+CREATE TABLE probationData
+(
+    id                 SERIAL,
+    probationJournalId INTEGER,
+    type               INTEGER,
+    link               TEXT
+);
 
 --changeset sirko:1
